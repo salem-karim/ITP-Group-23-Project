02bea973357afb95bb4a68fc96a7ef53655be38d
=Dominik Schrenk <dsschrenk@gmail.com>
2024-04-01 20:38:13 +0200

    Story etwas erweitert: Bierquest und Händer hinzugefügt

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 class Player {
-  constructor(name, health, strength, gold) {
+  constructor(name, health, strength, gold, sword, shield, beer, beerMission) {
     this.name = name;
     this.health = health;
     this.strength = strength;
     this.gold = gold
-    this.sword = false;
-    this.shield = false;
+    this.sword = sword;
+    this.shield = shield;
+    this.beer = beer;
+    this.beerMission = beerMission;
   }
 }
 
@@ -15,10 +17,12 @@ const optionButtonsElement = document.getElementById("option-buttons");
 const goldElement = document.getElementById("gold");
 const healthElement = document.getElementById("health");
 const strengthElement = document.getElementById("strength");
+const swordElement = document.getElementById("sword");
+const shieldElement = document.getElementById("shield");
 //Delcare empty state
 let state = {};
 //Initialize Player with default values
-const player = new Player("Player", 100, 10, 0);
+const player = new Player("Player", 100, 10, 0, "-", "-", false, false);
 
 
 function startGame() {
@@ -32,6 +36,8 @@ function updatePlayerStats() {
   goldElement.textContent = player.gold;
   healthElement.textContent = player.health;
   strengthElement.textContent = player.strength;
+  swordElement.textContent = player.sword;
+  shieldElement.textContent = player.shield;
 }
 
 function showTextNode(textNodeIndex) {
@@ -75,6 +81,13 @@ function selectOption(option) {
   state = Object.assign(state, option.setState);
   showTextNode(nextTextNodeId);
   updatePlayerStats();
+  debugLogs();
+}
+
+function debugLogs() {
+  console.log("Player: ", player);
+  console.log("State: ", state);
+
 }
 // Object array with all the text nodes
 const textNodes = [
@@ -101,10 +114,6 @@ const textNodes = [
         text: "Trink von einem unbekannten blauen Trank",
         setState: () => { player.health = 0; },
         nextText: 4,
-      },
-      {
-        text: "Erstmal umsehen und nach einer Tür suchen",
-        nextText: 7,
       },
     ],
   },
@@ -155,18 +164,7 @@ const textNodes = [
   },
   {
     id: 7,
-    text: "Du siehst eine Tür und hörst Stimmen dahinter. Was willst du tun?",
-    options: [
-      {
-        text: "Stürme lautstart den Raum und fordere die Stimmen heraus",
-        setState: () => { player.health = 0; },
-        nextText: 5,
-      },
-      {
-        text: "Warte bis die Stimmen verschwinden und öffne die Tür leise",
-        nextText: 6,
-      },
-    ],
+    nextText: 8,
   },
   {
     id: 8,
@@ -215,21 +213,130 @@ const textNodes = [
     text: "Du entkommst dem Wächter nur knapp, aber er hat dich verletzt. Du läufst erstmals richtung Stadt.",
     options: [
       {
-        text: "Restart",
-        nextText: -1,
+        text: "Weiter...",
+        nextText: 12,
       },
     ],
   },
   {
     id: 12,
-    text: "blabla",
+    text: "Du brauchst jetzt etwas Geld, Ausrüstung und volle Gesundheit. Du siehst ein paar Händler und andere Gestalten.",
     options: [
       {
-        text: "Restart",
-        nextText: -1,
+        text: "Zum Schmied gehen",
+        nextText: 13,
       },
+      {
+        text: "Zum mysteriösen Mann in der Ecke gehen, der dir zuwinkt",
+        nextText: 14,
+      },
+      {
+        text: "Zum Wirt gehen",
+        nextText: 15,
+      },
+      {
+        text: "Wieder zurück zum Schloss (benötigt 100 Gesundheit und 25 Stärke)",
+        requiredState: (player) => player.health >= 100 && player.strength >= 25,
+        nextText: 16,
+      }
     ],
   },
+  {
+    id: 13,
+    text: "Der Schmied bietet dir ein Anfängerschwert (30 Gold, +5 Stärke), ein Fortgeschrittenenschwert (100 Gold, +10 Stärke) und einen Eisenschild (50 Gold, +10 Gesundheit) an.",
+    options: [
+      {
+        text: "Kein Geld? Verlasse den Schmied",
+        nextText: 12,
+      },
+      {
+        text: "Ein Anfängerschwert kaufen (30 Gold)",
+        requiredState: (player) => player.gold >= 30,
+        setState: () => { player.gold -= 30; player.sword = "Anfängerschwert"; player.strength += 5; },
+        nextText: 12,
+      },
+      {
+        text: "Ein Fortgeschrittenenschwert kaufen (100 Gold)",
+        requiredState: (player) => player.gold >= 100,
+        setState: () => { player.gold -= 100; player.sword = "Fortgeschrittenenschwert"; player.strength += 10; },
+        nextText: 12,
+      },
+      {
+        text: "Einen Eisenschild kaufen (50 Gold)",
+        requiredState: (player) => player.gold >= 50,
+        setState: () => { player.gold -= 50; player.shield = "Eisenschild"; player.health += 10; },
+        nextText: 12,
+      },
+    ]
+    },
+    {
+      id: 14,
+      text: "Der Mann bietet dir für jede Mission 20 Gold an.\n1. Er sagt: Ich brauche ein Bier, aber ich kann nicht selbst gehen. Kannst du mir eins holen?\n 2. WEITERE MISSIONEN",
+      options: [
+        {
+          text: "Zurückgehen",
+          nextText: 12,
+        },
+        {
+          text: "Bier geben",
+          requiredState: (player) => player.beer == true,
+          setState: () => { player.gold += 20; player.beer = false; player.beerMission = true; },
+          nextText: 12,
+        },
+        {
+          text: "Nächste Mission...",
+          requiredState: (player) => player.beerMission == true,
+          nextText: 18,
+        }
+      ]
+    },
+    {  
+      id: 15,
+      text: "Der Wirt sieht sieht freundlich aus und ein Mann sitzt an einem runden Tisch. Du siehst auch eine Tafel mit Preisen. Ein Bier kostet 5 Gold, Essen kostet 5 Gold und gibt dir 10 Gesundheit.",
+      options: [
+        {
+          text: "Verlasse die Kneipe",
+          nextText: 12,
+        },
+        {
+          text: "Ein Bier für den Misteriösen Mann draußen kaufen (5 Gold)",
+          requiredState: (player) => player.gold >= 5 && player.beer == false && player.beerMission == false,
+          setState: () => { player.gold -= 5; player.beer = true; },
+          nextText: 15, 
+          
+        },
+        {
+          text: "Etwas essen (5 Gold, + 10 Gesundheit)",
+          requiredState: (player) => player.gold >= 5 && player.health <= 90 && player.beerMission == true,
+          setState: () => { player.gold -= 5; player.health += 10; },
+          nextText: 15
+        },
+        {
+          text: "Zu dem Mann am runden Tisch gehen",
+          nextText: 17
+        }
+      ]
+    },
+    {
+      id: 16,
+      text: "...",
+      nextText: 12
+    },
+    {
+      id: 17,
+      text: "Der Mann bietet Glücksspiel an. Traust du dich?",
+      options: [
+        {
+          text: "Verlasse die Kneipe",
+          nextText: 12
+        },
+        {
+          text: "EIN MINISPIEL ÜBERLEGEN",
+          nextText: 19
+        }
+      ]
+    },
+
 ];
 
 
